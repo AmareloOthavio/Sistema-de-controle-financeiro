bdf8b6aa09373741c9fc9c377cf400a4a704c99c
AmareloOthavio <167528849+AmareloOthavio@users.noreply.github.com>
2024-11-08 16:40:16 -0300

    Progresso back-end 1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 import fdb
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jqwdbjAHSDBhjBWd8723DWHV5rDWHD4783JHDKJVBWhdj'
-host = 'localhost'  # ou o IP do servidor onde o Firebird está rodando
+host = 'localhost'
 database = r'C:\Users\Aluno\Downloads\Banco - Sistema financeiro\SistemaFinanceiro.FDB'
 user = 'sysdba'
 password = 'sysdba'
@@ -36,5 +36,52 @@ class Receita:
         self.data = data
 
 
-#@app.route('/')
-#def cadastrar():
+@app.route('/')
+def index():
+    cursor1 = con.cursor()
+    cursor2 = con.cursor()
+    cursor1.execute('SELECT VALOR, DESCRICAO, DATA FROM DESPESAS')
+    despesas = cursor1.fetchall()
+    cursor2.execute('SELECT VALOR, FONTE, DATA FROM RECEITAS')
+    receitas = cursor2.fetchall()
+
+    cursor1.close()
+    cursor2.close()
+    return render_template('index.html', despesas=despesas, receitas=receitas)
+
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastrar():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        cursor = con.cursor()
+        try:
+            # Verificar se já foi criada uma conta neste e-mail
+            cursor.execute("SELECT 1 FROM USUARIOS WHERE EMAIL = ?", (email,))
+            if cursor.fetchone():  # Se existir algum registro
+                flash("Erro: Este e-mail já possui uma conta.", "error")
+                return redirect(url_for('cadastrar'))
+
+            # Inserir o novo usuário
+            cursor.execute("INSERT INTO USUARIOS (NOME, EMAIL, SENHA) VALUES (?, ?, ?)", (nome, email, senha))
+            con.commit()
+
+        except Exception as e:
+            # Em caso de erro no banco de dados:
+            flash(f"Erro ao cadastrar usuário: {str(e)}", "error")
+            return redirect(url_for('cadastrar'))
+        finally:
+            con.close()
+
+        # Após o sucesso, redireciona para a página inicial
+        return redirect(url_for('index'))
+
+    # Se for um GET, apenas renderize o formulário de cadastro
+    return render_template('cadastro.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
